645e7ba3b830adee9bb22ec2764b1a4cb7216f16
ndaemy <yuyaebean@gmail.com>
2022-05-14 23:53:30 +0900

    ✨ Create MainStack

---
+++ src/screens/RootStack.tsx
@@ -0,0 +1,13 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainStack from "src/screens/MainStack";
+import { RootStackParamList } from "src/screens/types";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="MainStack" component={MainStack} />
+    </Stack.Navigator>
+  );
+}
